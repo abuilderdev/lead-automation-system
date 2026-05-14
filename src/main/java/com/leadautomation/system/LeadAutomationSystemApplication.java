05208d6a5d96aa766f499a308cab6d9a21038f0e
abuilderdev <140002756+abuilderdev@users.noreply.github.com>
2026-05-14 20:59:34 +0200

    spring boot and gradle environment setup

---
+++ src/main/java/com/leadautomation/system/LeadAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leadautomation.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadAutomationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadAutomationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leadautomation/system/LeadAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadautomation.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadAutomationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
